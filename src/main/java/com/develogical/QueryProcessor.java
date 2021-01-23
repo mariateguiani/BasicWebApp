61f18e5f9c3b053d149b2c44777855fcdcf17b5c
George Ordish <george.ordish17@imperial.ac.uk>
2021-01-23 13:18:09 +0000

    Remove knowledge of Chatley

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -9,10 +9,10 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.contains("chatley")) {
-            return "Robert Chatley is a Doctor and is Deputy Director of undergrad studies and the world renowned" +
-                    "Imperial College London.";
-        }
+//        if (query.contains("chatley")) {
+//            return "Robert Chatley is a Doctor and is Deputy Director of undergrad studies and the world renowned " +
+//                    "Imperial College London.";
+//        }
         return "";
     }
 }
